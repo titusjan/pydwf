6f75ae7ff90e4821ab927f01f347a536ca51bfd2
Sidney Cadot <sidney@jigsaw.nl>
2019-12-31 08:50:26 +0100

    Prettyfied output.

--- EnumerateDigilentDevices.py
+++ EnumerateDigilentDevices.py
@@ -7,10 +7,10 @@ from pydwf import DigilentWaveformLibrary, DwfEnumConfigInfo
 def main():
 
     parser = argparse.ArgumentParser(description="Enumerate Digilent Discovery devices.")
-    parser.add_argument('-o', '--obsolete', action='store_true',
-                        help="for each device, retrieve and show analog-in parameters using obsolete FDwfEnumAnalogIn* API calls", dest='show_obsolete')
-    parser.add_argument('-c', '--configurations', action='store_true',
-                        help="for each device, show available configurations", dest='list_configurations')
+    parser.add_argument('--no-obsolete-api', action='store_false',
+                        help="for each device, suppress printing of analog-in parameters using obsolete FDwfEnumAnalogIn* API calls", dest='use_obsolete_api')
+    parser.add_argument('--no-configurations', action='store_false',
+                        help="for each device, suppress printing of configurations", dest='list_configurations')
 
     args = parser.parse_args()
 
@@ -19,39 +19,39 @@ def main():
     num_devices = dwf.enum.count()
     for device_index in range(num_devices):
 
-        print("Device index {} ({} of {} devices):".format(device_index, device_index+1, num_devices))
-        print()
         devtype = dwf.enum.deviceType(device_index)
         is_open = dwf.enum.deviceIsOpened(device_index)
         username = dwf.enum.userName(device_index)
         devicename = dwf.enum.deviceName(device_index)
         serial = dwf.enum.serialNumber(device_index)
 
-        print("    Basic device information")
-        print("    ========================")
+        header = "Device information for device #{} ({} of {} devices)".format(device_index, device_index+1, num_devices)
+
+        print(header)
+        print("=" * len(header))
         print()
-        print("    device .......... : {}".format(devtype[0]))
-        print("    version ......... : {}".format(devtype[1]))
-        print("    open ............ : {}".format(is_open))
-        print("    username ........ : {!r}".format(username))
-        print("    devicename ...... : {!r}".format(devicename))
-        print("    serial .......... : {!r}".format(serial))
+        print("  device .......... : {}".format(devtype[0]))
+        print("  version ......... : {}".format(devtype[1]))
+        print("  open ............ : {}".format(is_open))
+        print("  username ........ : {!r}".format(username))
+        print("  devicename ...... : {!r}".format(devicename))
+        print("  serial .......... : {!r}".format(serial))
         print()
 
-        if args.show_obsolete:
+        if args.use_obsolete_api:
 
             ai_channels = dwf.enum.analogInChannels(device_index)
             ai_bufsize = dwf.enum.analogInBufferSize(device_index)
             ai_bits = dwf.enum.analogInBits(device_index)
             ai_frequency = dwf.enum.analogInFrequency(device_index)
 
-            print("    Analog-in information (retrieved through obsolete API)")
-            print("    ======================================================")
+            print("Analog-in information (retrieved through obsolete API)")
+            print("------------------------------------------------------")
             print()
-            print("    number of channels ...... : {!r}".format(ai_channels))
-            print("    buffer size ............. : {!r}".format(ai_bufsize))
-            print("    bits .................... : {!r}".format(ai_bits))
-            print("    frequency ............... : {!r}".format(ai_frequency))
+            print("  number of channels ...... : {!r}".format(ai_channels))
+            print("  buffer size ............. : {!r}".format(ai_bufsize))
+            print("  bits .................... : {!r}".format(ai_bits))
+            print("  frequency ............... : {!r}".format(ai_frequency))
             print()
 
         if args.list_configurations:
@@ -65,13 +65,15 @@ def main():
                     configuration_parameter_value = dwf.enum.configInfo(configuration_index, configuration_parameter)
                     configuration_data[(configuration_index, configuration_parameter)] = configuration_parameter_value
 
-            print("    Supported configurations")
-            print("    ========================")
+            header = "Supported configurations ({})".format(num_config)
+
+            print(header)
+            print("-" * len(header))
             print()
-            print("    configuration_index:    {}".format("  ".join("{:8d}".format(configuration_index) for configuration_index in range(num_config))))
-            print("    ----------------------  {}".format("  ".join("--------" for configuration_index in range(num_config))))
+            print("  configuration_index:    {}".format("  ".join("{:8d}".format(configuration_index) for configuration_index in range(num_config))))
+            print("  ----------------------  {}".format("  ".join("--------" for configuration_index in range(num_config))))
             for configuration_parameter in DwfEnumConfigInfo:
-                print("    {:22}  {}".format(configuration_parameter.name, "  ".join("{:8d}".format(configuration_data[(configuration_index, configuration_parameter)]) for configuration_index in range(num_config))))
+                print("  {:22}  {}".format(configuration_parameter.name, "  ".join("{:8d}".format(configuration_data[(configuration_index, configuration_parameter)]) for configuration_index in range(num_config))))
             print()
 
 if __name__ == "__main__":
