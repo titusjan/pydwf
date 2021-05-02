9e6f47245738af5586d7b056d01857abcca3050e
Sidney Cadot <sidney@jigsaw.nl>
2021-05-02 10:53:02 +0200

    Release 0.3.1

--- pydwf/__init__.py
+++ pydwf/__init__.py
@@ -12,7 +12,7 @@ from typing import Optional, Tuple, List
 
 from .dwf_function_signatures import dwf_function_signatures, dwf_version as expected_dwf_version
 
-__version__ = "0.3.0"
+__version__ = "0.3.1"
 
 _HDWF_NONE = 0  # HDWF value representing a bad device handle.
 
